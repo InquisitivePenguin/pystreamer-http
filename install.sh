f02cdac478e5b7ddb5e7adb5015ef6814ecd0b93
Jackson Lewis <st.japa6@gmail.com>
2018-01-25 17:09:48 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get upgrade && \
+sudo apt-get install python-opencv && \
+git clone https://github.com/jacksoncoder/pystreamer-http && \
+cd pystreamer-http && \
+pip install flask && \
+echo "Pystreamer-HTTP has been successfully installed! Now, run 'python main.py' to start up the server" && \
+echo "Made by Jackson Lewis. Copyright 2018"
