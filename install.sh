da252493109fcac7abfa0745be8dd45c3b750bdd
Jackson Lewis <st.japa6@gmail.com>
2018-01-26 01:38:47 +0000

    Add modprobe

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo apt-get install python-opencv && \
 git clone https://github.com/jacksoncoder/pystreamer-http && \
 cd pystreamer-http && \
 pip install flask && \
+sudo modprobe bcm2835-v4l2 && \
 echo "Pystreamer-HTTP has been successfully installed! Now, run 'python main.py' to start up the server" && \
 echo "Made by Jackson Lewis. Copyright 2018"
